ab6541cd8ce7b038bf6f2bea6139163d03646fda
Bernabe <bernabrb@gmail.com>
2022-06-21 18:43:35 -0300

    D5 - Ejs y Pug
    Desafio 5, pate 2 y 3 correspondientes a Ejs y Pug

---
+++ desafio5/p3-pug/main.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const router = express.Router()
+const PORT = 8080
+const functions = require("./functions");
+const path = require('path')
+
+// Configuración static
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Pug
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+// Agrego producto
+router.post('/', (req, res) => {
+    let data = req.body
+    functions.save(data.name, data.price, data.thumbnail)
+    res.redirect('/')
+})
+
+// Devuelve todos los productos
+router.get('/', (req, res) => {
+    const products = functions.getAll()
+    res.render('viewProducts', {
+        products: products,
+        qProducts: products.length
+    })
+})
+
+// Atajo para cargar productos para facilitar pruebas
+router.get('/cargaFalsa', (req, res) => {
+    functions.save('Las Moras', 600, 'https://cdn1.iconfinder.com/data/icons/kitchen-and-food-2/44/wine-256.png')
+    functions.save('Otro loco mas', 500, 'https://cdn1.iconfinder.com/data/icons/drink-beverage/512/22-wine-drink-bottle-glass-256.png')
+    functions.save('Rutini', 1100, 'https://cdn0.iconfinder.com/data/icons/colored/94/Colored_Wine-256.png')
+    res.redirect('/')
+})
+
+app.use('/productos', router)
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
+})
+server.on("error", error => console.log(`Error en servidor ${error}`))
+
