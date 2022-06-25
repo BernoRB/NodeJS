1540e256e8f5fe6ba32023d85363bf922206c870
Bernabe <bernabrb@gmail.com>
2022-06-25 17:35:32 -0300

    Archivo
    Los endpoints de 'productos' están funcionales manejando todo con archivos

--- final-1ra/app.js
+++ final-1ra/app.js
@@ -3,7 +3,6 @@ const path = require('path')
 const http = require('http')
 const PORT = 8080
 
-// const index = require('./routes/index');
 const products = require('./routes/productsRou')
 const cart = require('./routes/cartRou')
 
@@ -13,26 +12,18 @@ const app = express()
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'twig');
 
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-//app.use('/', index);
 app.use('/api/carrito', cart)
 app.use('/api/productos', products)
-
-
-
-
-
 
 
 const server = app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${server.address().port}`)
 })
 server.on("error", error => console.log(`Error en servidor ${error}`))
-
 
 
 module.exports = app;
--- final-1ra/controllers/productController.js
+++ final-1ra/controllers/productController.js
@@ -1,16 +1,36 @@
+const fs = require('fs')
+const path = require('path');
+
 const Product = require("../models/product");
 
-// Luego esto es por archivo, seria tomarlo de ahi
-let products = []
+prodsPath = path.join(__dirname, '..', 'data', 'prodsDB.json')
 
-//router.get    ('/:id?',  product.getAllProducts)
+
+// Traer todos los productos del archivo
+function getAllP() {
+    try {
+        const prodsFile = fs.readFileSync(prodsPath, 'utf-8')
+        const prodsData = JSON.parse(prodsFile)
+        return prodsData
+    } catch (error) {
+        console.log('Error al obtener los productos', error)
+    }
+}
+
+// Guardar en el archivo
+function saveInFile(products) {
+    fs.writeFile(prodsPath, JSON.stringify(products), (err) => {
+        if (err) throw err
+        console.log('Archivo guardado correctamente')
+    })
+}
+
+//router.get('/:id?', product.getAllProducts)
 function getAllProducts(req, res) {
+    const products = getAllP()
     const id = req.params.id
     if (!id) {
-        const resp = (products.length > 0)
-        ? products
-        : { error: 'Aun no hay productos' }
-        res.json(resp)
+        res.json(products)
     } else {
         let product = products.find((element) => element.id == id)
         res.json(product || { error: 'Producto no encontrado' })
@@ -19,14 +39,16 @@ function getAllProducts(req, res) {
 
 //router.post('/', product.saveProduct)
 function saveProduct(req, res) {
+    const products = getAllP()
     let data = req.body
     const timestamp = 'asdtimestamp'
     let newId = 1
     if (!products.length == 0)
-        newId = (products[products.length-1].id) + 1 // Uno más que el último
+        newId = (products[products.length - 1].id) + 1 // Uno más que el último
     try {
         let product = new Product(newId, timestamp, data.title, data.desc, data.price, data.thumbnail, data.code, data.stock)
         products.push(product)
+        saveInFile(products)
         res.json(newId)
     } catch (error) {
         res.json('Ocurrio un error al intentar agregar el producto')
@@ -35,31 +57,35 @@ function saveProduct(req, res) {
 
 //router.put('/:id', product.modifyProduct)
 function modifyProduct(req, res) {
+    const products = getAllP()
     const data = req.body
     const timestamp = 'timestampMODIFICADA'
     const i = products.findIndex((element) => element.id == req.params.id)
     if (i != -1) {
-        products[i].timestamp = data.timestamp
-        products[i].title     = data.title
-        products[i].desc      = data.desc
-        products[i].price     = data.price
+        products[i].timestamp = timestamp
+        products[i].title = data.title
+        products[i].desc = data.desc
+        products[i].price = data.price
         products[i].thumbnail = data.thumbnail
-        products[i].code      = data.code
-        products[i].stock     = data.stock
+        products[i].code = data.code
+        products[i].stock = data.stock
+        saveInFile(products)
         res.json('Producto actualizado correctamente')
     } else
         res.json({ error: 'Producto no encontrado' })
 }
 
+
 //router.delete ('/:id',   product.deleteProduct)
 function deleteProduct(req, res) {
+    const products = getAllP()
     const i = products.findIndex((element) => element.id == req.params.id)
     if (i != -1) {
         products.splice(i, 1)
-        res.json({ ok: 'Producto eliminado correctamente'} )
+        saveInFile(products)
+        res.json({ ok: 'Producto eliminado correctamente' })
     } else
         res.json({ error: 'Producto no encontrado' })
 }
 
 module.exports = { getAllProducts, saveProduct, modifyProduct, deleteProduct }
-
