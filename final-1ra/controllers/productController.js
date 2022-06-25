c74585b1185efa9c0ff99a912126ae89a76d41bb
Bernabe <bernabrb@gmail.com>
2022-06-25 16:27:45 -0300

    Producto
    Las rutas de productos funcionan y todo se hace, aun sin persistencia en memoria con archivo

--- final-1ra/controllers/productController.js
+++ final-1ra/controllers/productController.js
@@ -5,22 +5,25 @@ let products = []
 
 //router.get    ('/:id?',  product.getAllProducts)
 function getAllProducts(req, res) {
-    var id = req.params.id
+    const id = req.params.id
     if (!id) {
-        res.json('TODOS LOS PRODUCTOS, NO HAY ID')
+        const resp = (products.length > 0)
+        ? products
+        : { error: 'Aun no hay productos' }
+        res.json(resp)
     } else {
-        res.json(`PRODUCTO CON ID ${id}`)
+        let product = products.find((element) => element.id == id)
+        res.json(product || { error: 'Producto no encontrado' })
     }
 }
 
-//router.post   ('/',      product.saveProduct)
+//router.post('/', product.saveProduct)
 function saveProduct(req, res) {
     let data = req.body
     const timestamp = 'asdtimestamp'
     let newId = 1
-    if (!products.length == 0) {
+    if (!products.length == 0)
         newId = (products[products.length-1].id) + 1 // Uno más que el último
-    }
     try {
         let product = new Product(newId, timestamp, data.title, data.desc, data.price, data.thumbnail, data.code, data.stock)
         products.push(product)
@@ -30,14 +33,32 @@ function saveProduct(req, res) {
     }
 }
 
-//router.put    ('/:id',   product.modifyProduct)
+//router.put('/:id', product.modifyProduct)
 function modifyProduct(req, res) {
-    res.json('entrado a modifyProduct')
+    const data = req.body
+    const timestamp = 'timestampMODIFICADA'
+    const i = products.findIndex((element) => element.id == req.params.id)
+    if (i != -1) {
+        products[i].timestamp = data.timestamp
+        products[i].title     = data.title
+        products[i].desc      = data.desc
+        products[i].price     = data.price
+        products[i].thumbnail = data.thumbnail
+        products[i].code      = data.code
+        products[i].stock     = data.stock
+        res.json('Producto actualizado correctamente')
+    } else
+        res.json({ error: 'Producto no encontrado' })
 }
 
 //router.delete ('/:id',   product.deleteProduct)
 function deleteProduct(req, res) {
-    res.json('entrado a deleteProduct')
+    const i = products.findIndex((element) => element.id == req.params.id)
+    if (i != -1) {
+        products.splice(i, 1)
+        res.json({ ok: 'Producto eliminado correctamente'} )
+    } else
+        res.json({ error: 'Producto no encontrado' })
 }
 
 module.exports = { getAllProducts, saveProduct, modifyProduct, deleteProduct }
