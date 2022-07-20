d57c1b49a17b2d74d28af50224b56157d4c1677b
Bernabe <bernabrb@gmail.com>
2022-07-20 11:16:29 -0300

    Primer
    Arranco desafio 12. Base del 11. Listo Yarg.

---
+++ desafio12/server.js
@@ -0,0 +1,148 @@
+require('dotenv').config()
+const path = require("path")
+const express = require('express')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const passport = require('passport')
+const { initializePassport } = require('./src/utils/passport.config.js')
+
+const products = require("./src/controllers/controllerProds")
+const { Server: IOServer } = require("socket.io")
+const { Server: HttpServer } = require("http")
+
+const { engine } = require("express-handlebars")
+
+
+// Yargs desafio 12
+const yargs = require('yargs/yargs')(process.argv.slice(2))
+const argv = yargs.options({
+  'port': {
+    alias: 'p',
+    describe: 'provide a port, default 8080',
+    default: '8080'
+  }
+})
+.argv
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "public")))
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
+httpServer.listen(argv.port, () => console.log(`Servidor escuchando en el puerto ${argv.port}`))
+
+// Sessions, passport
+let baseSession = session({
+  store: MongoStore.create({ mongoUrl: process.env.MONGOURL }),
+  mongoOptions: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  secret: 'Desafio11',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {maxAge: 600_000}
+})
+
+app.use(express.json())
+app.use(baseSession)
+initializePassport()
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+// Views engine
+app.engine(
+  "hbs",
+  engine({
+    defaultLayout: "index.hbs",
+    extname: ".hbs",
+  })
+)
+app.set("views", path.join(__dirname, "public"))
+app.set("view engine", "hbs")
+
+
+// Socket
+io.on("connection", async (socket) => {
+  socket.emit("products", await products.getAllProducts())
+  socket.on("new-product", async (newProduct) => {
+    await products.addProduct(newProduct)
+    const prods = await products.getAllProducts()
+    io.sockets.emit("products", prods)
+  })
+})
+
+
+// Rutas
+app.get('/', passport.authenticate('login', { failureRedirect: '/login' }), (req, res) => {
+  res.redirect("/dashboard")
+})
+
+app.get('/dashboard', (req, res) => {
+  if (req.user) {
+    res.render("dash", {
+      username: req.user.username,
+      email: req.user.email,
+      loggedIn: true
+    })
+  } else
+    res.redirect('/login')
+})
+
+app.get('/login', (req, res) => {
+  if (req.user) //si ya esta logeado y entra aca lo mandamos al dash
+    res.redirect('dashboard')
+  res.render('login')
+})
+
+app.post('/login', passport.authenticate('login', { failureRedirect: '/retrylogin' }), (req, res) => {
+  res.redirect('/dashboard')
+})
+
+// Si llega porque logeó mal
+app.get('/retrylogin', (req, res) => {
+  res.render("login", {
+    retry: true
+  })
+})
+
+app.get('/signup', (req, res) => {
+  if (req.user) //si ya esta logeado y entra aca lo mandamos al dash
+    res.redirect('dashboard')
+  res.render('signup')
+})
+
+app.post('/signup', passport.authenticate('register', { failureRedirect: '/retrysignup' }), (req, res) => {
+  res.redirect('/dashboard')
+})
+
+app.get('/retrysignup', (req, res) => {
+  res.render("signup", {
+    retry: true
+  })
+})
+
+app.get('/logout', (req, res) => {
+  nameLogout = req.user.username
+  req.logout((err) => {
+    if (err) {
+      res.redirect('/login')
+    }
+    else
+      res.render("logout", {
+        nameLogout
+      })
+  })
+})
+
+
+/*
+app.get('/', sessionChecker, (req, res) => {
+  res.redirect("/login")
+})
+app.get('/login', sessionChecker, login)
+app.post('/login', loginPost)
+app.get('/logout', logout)
+app.get('/dash', dash)
+*/
---
+++ desafio12/src/controllers/controllerDBprods.js
@@ -0,0 +1,44 @@
+// Las funciones de este modulo interactuan directamente con la base de datos de productos.
+const db = require('../sqlite/config.js')
+const tabla = 'productos'
+
+const tableExists = async () => {
+    return await db.schema.hasTable(tabla)
+}
+
+const createTable = async () => {
+    try {
+        await db.schema.createTable(tabla, table => {
+            table.increments()
+            table.text('title', 255)
+            table.real('price')
+            table.text('thumbnail', 255)
+            table.text('fecha', 255)
+        })
+    }
+    catch (error) {
+        throw error
+    }
+}
+
+const addProd = async (newProd) => {
+    try {
+        const { title, price, thumbnail } = newProd
+        const fecha = new Date().toLocaleString()
+        await db.insert({ title, price, thumbnail, fecha }).into(tabla)
+    }
+    catch (error) {
+        throw error
+    }
+}
+
+const getAll = async () => {
+    try {
+        return await db.select().table(tabla)
+    }
+    catch (error) {
+        throw error
+    }
+}
+
+module.exports = { tableExists, createTable, addProd, getAll }
